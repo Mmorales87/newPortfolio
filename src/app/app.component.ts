134915658cf7ae4b5a42574ac43f0dcf6cf3f55c
Marc Morales <marc.morales@honei.app>
2023-09-02 19:50:05 +0200

    save before update for bootstrap issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+import { HeaderComponent } from './shared/header/header.component'; 
+import { MainPageComponent } from './main-page/main-page.component'; 
+import { FooterComponent } from './shared/footer/footer.component'; 
+
+@NgModule({
+  declarations: [
+    HeaderComponent, // Asegúrate de declarar 'HeaderComponent' aquí
+    MainPageComponent, // Asegúrate de declarar 'MainPageComponent' aquí
+    FooterComponent, // Asegúrate de declarar 'FooterComponent' aquí
+  ],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent]
 })
+
 export class AppComponent {
   title = 'Marc Morales Garcia | Backend developer';
 }
