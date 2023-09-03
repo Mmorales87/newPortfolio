3589c02b96d7b7332106b3088f102b457ca10c43
Marc Morales <marc.morales@honei.app>
2023-09-03 19:16:41 +0200

    close dropdown after click

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent implements OnInit{
-
+export class MainPageComponent implements OnInit {
 
   customOptions: OwlOptions = {
     loop: true,
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, AfterViewInit, ElementRef, ViewChild, Renderer2, Inject } from '@angular/core';
 import * as $ from 'jquery';
 
 @Component({
@@ -10,17 +11,25 @@ export class HeaderComponent implements AfterViewInit {
   @ViewChild('navbarToggle') navbarToggle!: ElementRef;
   @ViewChild('dropdownMenu') dropdownMenu!: ElementRef;
 
-  constructor() { }
+  constructor(private renderer: Renderer2, @Inject(DOCUMENT) private document: Document) { }
 
   ngAfterViewInit() {
-    // Burger Menu
-    const burgerMenu = () => {
-      this.navbarToggle.nativeElement.addEventListener('click', (event: Event) => {
-        event.preventDefault();
-        this.dropdownMenu.nativeElement.classList.toggle('show');
-      });
-    }
-
-    burgerMenu();
-  }
+        // Burger Menu
+        const burgerMenu = () => {
+          this.navbarToggle.nativeElement.addEventListener('click', (event: Event) => {
+            event.preventDefault();
+            this.dropdownMenu.nativeElement.classList.toggle('show');
+          });
+    
+          // Close the dropdown when a menu item is clicked
+          const menuItems = this.document.querySelectorAll('.nav-item');
+          menuItems.forEach((item) => {
+            item.addEventListener('click', () => {
+              this.dropdownMenu.nativeElement.classList.remove('show');
+            });
+          });
+        }
+    
+        burgerMenu();
+      }
 }
